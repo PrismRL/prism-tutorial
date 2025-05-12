075570c3cf1f5230d3033cb3aabaa136d4bddc4e
Matthew Blanchard <matthewrblanchard@gmail.com>
2025-05-12 14:06:57 -0400

    Added Void component and rewrote Fall to check for it.

--- modules/MyGame/actions/fall.lua
+++ modules/MyGame/actions/fall.lua
@@ -1,17 +1,21 @@
 --- @class Fall : Action
 local Fall = prism.Action:extend "Fall"
 
-local flyMask = prism.Collision.createBitmaskFromMovetypes({"fly"})
 --- @param level Level
 function Fall:_canPerform(level)
    local x, y = self.owner:getPosition():decompose()
-   local cellMask = level:getCell(x, y):getCollisionMask()
+   local cell = level:getCell(x, y)
+
+   -- We can only fall on cells that are voids.
+   if not cell:hasComponent(prism.components.Void) then return false end
+
+   local cellMask = cell:getCollisionMask()
    local mover = self.owner:getComponent(prism.components.Mover)
    local mask = mover and mover.mask or 0 -- default to the immovable mask
 
-   local isCellFlying = prism.Collision.checkBitmaskOverlap(flyMask, cellMask)
-   local canActorMoveHere = prism.Collision.checkBitmaskOverlap(cellMask, mask)
-   return isCellFlying and not canActorMoveHere
+   -- We have a Void component on the cell. If the actor CAN'T move here
+   -- then they fall.
+   return not prism.Collision.checkBitmaskOverlap(cellMask, mask)
  end
 
 --- @param level Level
---
+++ modules/MyGame/components/cell/void.lua
@@ -0,0 +1,6 @@
+--- @class Void : Component
+--- @field mask Bitmask
+local Void = prism.Component:extend("Void")
+Void.name = "Void"
+
+return Void
